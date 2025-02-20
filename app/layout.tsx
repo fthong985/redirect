91d4d85bbada666b746d9f3c9fe5edb3b8451ae8
kenneth <kennethdean08@gmail.com>
2025-02-21 00:43:30 +0800

    add adsense meta tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,6 +24,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta name="google-adsense-account" content="ca-pub-9998948536991634" />
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
