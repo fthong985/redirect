c8145baceffe7aee7655ac4ca86b72502023d021
kenneth <kennethdean08@gmail.com>
2025-03-07 20:25:45 +0800

    update to main landing page

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,8 @@
-import Link from "next/link";
+"use client";
+import { useRouter } from "next/navigation";
 import React from "react";
 export default function Footer() {
+  const router = useRouter();
   return (
     <footer className="text-white w-full py-8 px-1 lg:px-4 border-t border-[#4e4d4d] text-center mt-16  self-end ">
       <p className="text-xs font-light flex flex-col ">
@@ -16,13 +18,21 @@ export default function Footer() {
       </p>
 
       <div className="flex gap-3 justify-center mt-2">
-        <Link href={"/privacy-and-policy"} className="underline">
+        <button
+          className="underline"
+          onClick={() =>
+            router.push(`${process.env.NEXT_PUBLIC_URL}/privacy-and-policy`)
+          }
+        >
           Privacy and Policy.
-        </Link>
+        </button>
 
-        <Link href={"/dmca"} className="underline">
+        <button
+          className="underline"
+          onClick={() => router.push(`${process.env.NEXT_PUBLIC_URL}/dmca`)}
+        >
           DMCA
-        </Link>
+        </button>
       </div>
     </footer>
   );
